53327b257a137cd4b56a66215b01cb961aed793a
sharonpalivela <palivelasharon@gmail.com>
2025-05-26 16:31:25 +0530

    Add backend code for deployment

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-
 const protect = require('../middleware/authMiddleware');
 const WorkoutLog = require('../models/WorkoutLog.model');   
 
